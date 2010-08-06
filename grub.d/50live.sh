71be0f71bcb5163c5367e147f4d7013e4590162e
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-08-06 17:53:21 +0900

    add SUDO_USER as username

--- grub.d/50live.sh
+++ grub.d/50live.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
+LIVE_OPTIONS="boot=casper nopersistent rw quiet splash"
+if [ -n "$SUDO_USER" ]; then
+	LIVE_OPTIONS="$LIVE_OPTIONS username=$SUDO_USER"
+fi
 cat <<EOF
 menuentry "グラフィカルモードでライブ環境を起動する (Start Live GNU/Linux in Graphical Mode)" {
-	linux	/boot/vmlinuz boot=casper nopersistent rw quiet splash
+	linux	/boot/vmlinuz $LIVE_OPTIONS
 	initrd	/boot/initrd.img
 }
 menuentry "セーフグラフィカルモードでライブ環境を起動する (Start Live GNU/Linux in Safe Graphical Mode)" {
-	linux	/boot/vmlinuz boot=casper nopersistent xforcevesa rw quiet splash
+	linux	/boot/vmlinuz $LIVE_OPTIONS xforcevesa
 	initrd	/boot/initrd.img
 }
 menuentry "ディスクの破損をチェックする (Check disc for defects)" {
