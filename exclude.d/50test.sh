58b62458021671cd66dc7aa9552e451af5605642
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-08-15 10:08:37 +0900

    exclude if LOCAL_TEST

--- exclude.d/50test.sh
+++ exclude.d/50test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ "${LOCAL_TEST:+set}" != set ]; then
+	exit
+fi
 {
 	echo "/usr/share/doc/*" # exclude documents when test
 	echo "/usr/src/*" # exclude src when test
--- exclude.d/50vmware.sh
+++ exclude.d/50vmware.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ "${LOCAL_TEST:+set}" != set ]; then
+	exit
+fi
 {
 	echo "/usr/lib/vmware-tools/modules/binary"
 } | xargs -n1 -- printf -- '- %s\n'
