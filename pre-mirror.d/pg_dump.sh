4e39c5385b997c1c1d4970e568b969030da5f759
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-08-19 13:15:45 +0900

    add pre-mirror hook

---
+++ pre-mirror.d/pg_dump.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -eu
+
+umask 077
+PG_BACKUP_DIR=${PG_BACKUP_DIR:-/home/pg_backup}
+PG_DUMP="/usr/bin/pg_dump -O" 
+
+if [ -f /etc/default/mk-live-recovery ]; then
+    . /etc/default/mk-live-recovery
+fi
+
+postgresql_dump_all () {
+    if ! id -u postgres >/dev/null 2>&1; then
+	echo postgres not installed
+	return
+    fi
+
+    PG_DATABASES=$( su postgres -c "psql -c 'SELECT datname FROM pg_database;'" | egrep '^ [^ ]' | egrep -v '^ template[01]$|^ postgres$' )
+
+    mkdir -p "$PG_BACKUP_DIR"
+    su postgres -c "psql -l" >"$PG_BACKUP_DIR/psql-l.txt" 
+    for db_name in $PG_DATABASES; do
+        su postgres -c "$PG_DUMP $db_name" >"$PG_BACKUP_DIR/$db_name.pg_dump" 
+    done
+}
+
+case "$1" in
+    data)
+	set -x
+	postgresql_dump_all
+	;;
+esac
