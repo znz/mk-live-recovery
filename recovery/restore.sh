d09ada0c431831e0dc7f41801bd355f541060f1c
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-08-23 19:11:51 +0900

    use files in current dir if exist

--- recovery/restore.sh
+++ recovery/restore.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-cd $(dirname $0)
-
 export LIVE_RECOVERY_MODE="${LIVE_RECOVERY_MODE:-true}"
-export SFDISK_D=${SFDISK_D:-$(pwd)/sfdisk-d.txt}
+export SFDISK_D=${SFDISK_D:-/cdrom/recovery/sfdisk-d.txt}
 export TARGET_DRIVE=${TARGET_DRIVE:-/dev/sda}
 export MOUNTPOINT=${MOUNTPOINT:-/target}
 export SRC_DIR=${SRC_DIR:-/rofs}
+export WORKDIR=$(mktemp -d /tmp/restore.XXXXXXXXXX)
 
-/usr/sbin/format-and-mirror-copy live
+umount $MOUNTPOINT || :
+if [ -f ./format-and-mirror-copy ]; then
+    exec ./format-and-mirror-copy live
+else
+    exec /usr/sbin/format-and-mirror-copy live
+fi
