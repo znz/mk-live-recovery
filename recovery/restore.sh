f570b0093468c01097b2b57506b38d3e122de161
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2010-08-23 17:48:25 +0900

    set LIVE_RECOVERY_MODE

--- recovery/restore.sh
+++ recovery/restore.sh
@@ -3,8 +3,9 @@ set -e
 
 cd $(dirname $0)
 
+export LIVE_RECOVERY_MODE="${LIVE_RECOVERY_MODE:-true}"
 export SFDISK_D=${SFDISK_D:-$(pwd)/sfdisk-d.txt}
-export TARGET_DRIVE=${TARGET_DRIVE:-/dev/sdb}
+export TARGET_DRIVE=${TARGET_DRIVE:-/dev/sda}
 export MOUNTPOINT=${MOUNTPOINT:-/target}
 export SRC_DIR=${SRC_DIR:-/rofs}
 
